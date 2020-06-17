aa57ffd7fa77959d365a8fd89ad308edea1c892c
hu lanlan, (lanlan.hu@partner.bmw.com) <lanlan.hu@partner.bmw.com>
2020-06-17 10:38:58 +0800

    init

--- app/src/main/java/com/momochen/uidemo/scrollview/ExternalScrollView.kt
+++ app/src/main/java/com/momochen/uidemo/scrollview/ExternalScrollView.kt
@@ -3,13 +3,50 @@ package com.momochen.uidemo.scrollview
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
 import android.widget.ScrollView
+import com.momochen.uidemo.R
+import kotlinx.android.synthetic.main.activity_scrollview1.view.*
 
 class ExternalScrollView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ScrollView(context, attrs, defStyleAttr) {
 
+    var mLastY = 0
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        println("=========ExternalScrollView  dispatchTouchEvent")
+        return super.dispatchTouchEvent(ev)
+    }
+
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        return false
+        println("=========ExternalScrollView  onInterceptTouchEvent ${ev.toString()}")
+        ev ?: return super.onInterceptTouchEvent(ev)
+
+        var isIntercept = false
+
+        val y = ev.rawY
+        when (ev.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                return super.onInterceptTouchEvent(ev)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val detY = y - mLastY
+
+                val contentView: View? =
+                    findViewById(R.id.inside_scrollview) ?: return super.onInterceptTouchEvent(ev)
+
+                //判断子ScrollView是否滑动到顶部或者顶部
+                contentView?.let { view ->
+                    val isChildScrolledTop = detY > 0 && !view.canScrollVertically(-1)
+                    val isChildScrolledBottom = detY < 0 && !view.canScrollVertically(1)
+                    isIntercept = isChildScrolledTop || isChildScrolledBottom
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                isIntercept = false
+            }
+        }
+        return isIntercept
     }
 }
--- app/src/main/java/com/momochen/uidemo/scrollview/InsideScrollView.kt
+++ app/src/main/java/com/momochen/uidemo/scrollview/InsideScrollView.kt
@@ -8,13 +8,40 @@ import android.widget.ScrollView
 class InsideScrollView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ScrollView(context, attrs, defStyleAttr) {
+    private var mLastY = 0F
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        return super.dispatchTouchEvent(ev)
+        println("=========InsideScrollView  dispatchTouchEvent")
+        ev ?: return super.dispatchTouchEvent(ev)
 
+        val y = ev.y
+        when (ev.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                parent.requestDisallowInterceptTouchEvent(true)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val detY = y - mLastY
+                val isScrolledTop = detY > 0 && !canScrollVertically(-1)
+                val isScrolledBottom = detY < 0 && !canScrollVertically(1)
+                //根据自身是否滑动到顶部或者顶部来判断让父View拦截触摸事件
+                if (isScrolledTop || isScrolledBottom) {
+                    parent.requestDisallowInterceptTouchEvent(false);
+                }
+            }
+        }
+        mLastY = y
+        return super.dispatchTouchEvent(ev);
     }
 
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        println("=========InsideScrollView  onInterceptTouchEvent ${ev.toString()}")
+//        if (ev?.actionMasked == MotionEvent.ACTION_DOWN) {
+//            super.onInterceptTouchEvent(ev)
+//            return false
+//        }
+//
+//        return true
+
         return super.onInterceptTouchEvent(ev)
     }
 }
